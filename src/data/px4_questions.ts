27e9fd829c5786dc677efe1ca54353dcb8d867b3
virgopham96-sudo <virgopham96@gmail.com>
2026-05-26 15:59:51 +0700

    fix(data): update correct answer for question 118

--- src/data/px4_questions.ts
+++ src/data/px4_questions.ts
@@ -213,7 +213,7 @@ export const px4MultipleChoiceQuestions: Question[] = [
       { id: "C", text: "Trên 10%" },
       { id: "D", text: "Trên 15%" }
     ],
-    correctAnswer: "C",
+    correctAnswer: "A",
     type: 'multiple-choice'
   },
   {
